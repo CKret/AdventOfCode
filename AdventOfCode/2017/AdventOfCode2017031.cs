037ad4661ff24bb573b0b2b745aec3116437ac11
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-02 20:14:00 +0100

    Prepared for 2017 Day 3

---
+++ AdventOfCode/2017/AdventOfCode2017031.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode._2017
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2017, 3, 1, "", 0)]
+    public class AdventOfCode2017031 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+        }
+    }
+}
---
+++ AdventOfCode/2017/AdventOfCode2017032.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode._2017
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2017, 3, 2, "", 0)]
+    public class AdventOfCode2017032 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+        }
+    }
+}
