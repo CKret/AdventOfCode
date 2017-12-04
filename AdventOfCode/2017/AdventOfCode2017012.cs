e4819d03c48a1aa58af373a7f799a77f18ea9fd1
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-04 20:31:15 +0100

    Fixed summary for all problems.

--- AdventOfCode/2017/AdventOfCode2017012.cs
+++ AdventOfCode/2017/AdventOfCode2017012.cs
@@ -1,14 +1,37 @@
 ﻿using System.Linq;
 using AdventOfCode.Core;
+using Xunit;
 
 namespace AdventOfCode._2017
 {
     /// <summary>
-    /// Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list.
-    /// That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it.
-    /// Fortunately, your list has an even number of elements.
+    /// --- Day 1: Inverse Captcha ---
+    /// 
+    /// --- Part Two ---
+    /// 
+    /// You notice a progress bar that jumps to 50% completion. Apparently, the
+    /// door isn't yet satisfied, but it did emit a star as encouragement. The
+    /// instructions change:
+    /// 
+    /// Now, instead of considering the next digit, it wants you to consider the
+    /// digit halfway around the circular list. That is, if your list contains 10
+    /// items, only include a digit in your sum if the digit 10/2 = 5 steps forward
+    /// matches it. Fortunately, your list has an even number of elements.
+    /// 
+    /// For example:
+    /// 
+    ///     - 1212 produces 6: the list contains 4 items, and all four digits match
+    ///       the digit 2 items ahead.
+    ///     - 1221 produces 0, because every comparison is between a 1 and a 2.
+    ///     - 123425 produces 4, because both 2s match each other, but no other
+    ///       digit has a match.
+    ///     - 123123 produces 12.
+    ///     - 12131415 produces 4.
+    /// 
+    /// What is the solution to your new captcha?
+    /// 
     /// </summary>
-    [AdventOfCode(2017, 1, 2, "Find the sum of all digits that match the digit halfway around in the list. The list is circular.", 1292)]
+    [AdventOfCode(2017, 1, 2, "Inverse Captcha - Part Two", 1292)]
     public class AdventOfCode2017012 : AdventOfCodeBase
     {
         public override void Solve()
--- AdventOfCode/2017/AdventOfCode2017032.cs
+++ AdventOfCode/2017/AdventOfCode2017032.cs
@@ -4,26 +4,40 @@ using AdventOfCode.Core;
 namespace AdventOfCode._2017
 {
     /// <summary>
-    /// As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1.
-    /// Then, in the same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+    /// --- Day 3: Spiral Memory ---
+    /// 
+    /// --- Part Two ---
+    /// 
+    /// As a stress test on the system, the programs here clear the grid and then
+    /// store the value 1 in square 1.  Then, in the same allocation order as shown
+    /// above, they store the sum of the values in all adjacent squares, including
+    /// diagonals.
     /// 
     /// So, the first few squares' values are chosen as follows:
     /// 
-    ///     Square 1 starts with the value 1.
-    ///     Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
-    ///     Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
-    ///     Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
-    ///     Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+    ///     - Square 1 starts with the value 1.
+    ///     - Square 2 has only one adjacent filled square (with value 1), so it
+    ///       also stores 1.
+    ///     - Square 3 has both of the above squares as neighbors and stores the sum
+    ///       of their values, 2.
+    ///     - Square 4 has all three of the aforementioned squares as neighbors and
+    ///       stores the sum of their values, 4.
+    ///     - Square 5 only has the first and fourth squares as neighbors, so it
+    ///       gets the value 5.
     /// 
-    /// Once a square is written, its value does not change. Therefore, the first few squares would receive the following values:
+    /// Once a square is written, its value does not change. Therefore, the first
+    /// few squares would receive the following values:
     /// 
     /// 147  142  133  122   59
     /// 304    5    4    2   57
     /// 330   10    1    1   54
     /// 351   11   23   25   26
     /// 362  747  806---&gt;   ...
+    /// 
+    /// What is the first value written that is larger than your puzzle input?
+    /// 
     /// </summary>
-    [AdventOfCode(2017, 3, 2, "What is the first value written that is larger than your puzzle input?", 363010)]
+    [AdventOfCode(2017, 3, 2, "Spiral Memory - Part Two", 363010)]
     public class AdventOfCode2017032 : AdventOfCodeBase
     {
         public override void Solve()
