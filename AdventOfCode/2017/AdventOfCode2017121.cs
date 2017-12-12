02aa637185f475356cb5ef89352fe433d28dd3c8
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-12 17:55:27 +0100

    Added summary to 2017 Day 12.

--- AdventOfCode/2017/AdventOfCode2017121.cs
+++ AdventOfCode/2017/AdventOfCode2017121.cs
@@ -7,8 +7,58 @@ using AdventOfCode.Core;
 namespace AdventOfCode._2017
 {
     /// <summary>
+    /// --- Day 12: Digital Plumber ---
+    /// 
+    /// Walking along the memory banks of the stream, you find a small village that
+    /// is experiencing a little confusion: some programs can't communicate with
+    /// each other.
+    /// 
+    /// Programs in this village communicate using a fixed system of pipes.
+    /// Messages are passed between programs using these pipes, but most programs
+    /// aren't connected to each other directly. Instead, programs pass messages
+    /// between each other until the message reaches the intended recipient.
+    /// 
+    /// For some reason, though, some of these messages aren't ever reaching their
+    /// intended recipient, and the programs suspect that some pipes are missing.
+    /// They would like you to investigate.
+    /// 
+    /// You walk through the village and record the ID of each program and the IDs
+    /// with which it can communicate directly (your puzzle input). Each program
+    /// has one or more programs with which it can communicate, and these pipes are
+    /// bidirectional; if 8 says it can communicate with 11, then 11 will say it
+    /// can communicate with 8.
+    /// 
+    /// You need to figure out how many programs are in the group that contains
+    /// program ID 0.
+    /// 
+    /// For example, suppose you go door-to-door like a travelling salesman and
+    /// record the following list:
+    /// 
+    /// 0 &lt;-&gt; 2
+    /// 1 &lt;-&gt; 1
+    /// 2 &lt;-&gt; 0, 3, 4
+    /// 3 &lt;-&gt; 2, 4
+    /// 4 &lt;-&gt; 2, 3, 6
+    /// 5 &lt;-&gt; 6
+    /// 6 &lt;-&gt; 4, 5
+    /// 
+    /// In this example, the following programs are in the group that contains
+    /// program ID 0:
+    /// 
+    ///     - Program 0 by definition.
+    ///     - Program 2, directly connected to program 0.
+    ///     - Program 3 via program 2.
+    ///     - Program 4 via program 2.
+    ///     - Program 5 via programs 6, then 4, then 2.
+    ///     - Program 6 via programs 4, then 2.
+    /// 
+    /// Therefore, a total of 6 programs are in this group; all but program 1,
+    /// which has a pipe that connects it to itself.
+    /// 
+    /// How many programs are in the group that contains program ID 0?
+    /// 
     /// </summary>
-    [AdventOfCode(2017, 12, 1, "", 134)]
+    [AdventOfCode(2017, 12, 1, "Digital Plumber - Part One", 134)]
     public class AdventOfCode2017121 : AdventOfCodeBase
     {
         public override void Solve()
