9085c2c1b85597869ee5448df70b4dffd6c94f38
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-16 02:27:38 +0100

    Used MoreLinq to clean up KnotHash even more.

--- AdventOfCode/Core/Cryptography/HashAlgorithm.cs
+++ AdventOfCode/Core/Cryptography/HashAlgorithm.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using MoreLinq;
 
 namespace AdventOfCode.Core.Cryptography
 {
@@ -8,24 +9,20 @@ namespace AdventOfCode.Core.Cryptography
         public static IEnumerable<byte> KnotHash(string input, int rounds = 64)
         {
             int skipSize = 0, pos = 0;
-            var row = input.Select(c => (byte) c).ToList();
-            row.AddRange(new byte[] { 17, 31, 73, 47, 23 });
+            var row = input.Select(c => (byte) c).Concat(new byte[] { 17, 31, 73, 47, 23 }).ToList();
 
             var sparseHash = Enumerable.Range(0, 256).Select(i => (byte) i).ToArray();
 
-            for (var round = 0; round < rounds; round++)
+            foreach (var sequenceLength in row.Repeat(rounds))
             {
-                foreach (var sequenceLength in row)
+                for (var i = 0; i < sequenceLength / 2; i++)
                 {
-                    for (var i = 0; i < sequenceLength / 2; i++)
-                    {
-                        var tmp = sparseHash[(sequenceLength - i - 1 + pos) % sparseHash.Length];
-                        sparseHash[(sequenceLength - i - 1 + pos) % sparseHash.Length] = sparseHash[(pos + i) % sparseHash.Length];
-                        sparseHash[(pos + i) % sparseHash.Length] = tmp;
-                    }
-
-                    pos = (pos + sequenceLength + skipSize++) % sparseHash.Length;
+                    var tmp = sparseHash[(sequenceLength - i - 1 + pos) % sparseHash.Length];
+                    sparseHash[(sequenceLength - i - 1 + pos) % sparseHash.Length] = sparseHash[(pos + i) % sparseHash.Length];
+                    sparseHash[(pos + i) % sparseHash.Length] = tmp;
                 }
+
+                pos = (pos + sequenceLength + skipSize++) % sparseHash.Length;
             }
 
             return Enumerable.Range(0, 16).Select(i => sparseHash.Skip(16 * i).Take(16).Aggregate((curr, b) => (byte)(curr ^ b))).ToArray();
