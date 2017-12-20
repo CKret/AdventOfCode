1dee508493f2b1e2b31a33a1b3032cae0ac184aa
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-20 20:45:38 +0100

    Completed 2015 Day 15.

---
+++ AdventOfCode/2015/AdventOfCode2015161.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode._2015
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2015, 16, 1, "", null)]
+    public class AdventOfCode2015161 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+        }
+    }
+}
---
+++ AdventOfCode/2015/AdventOfCode2015162.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode._2015
+{
+    /// <summary>
+   /// </summary>
+    [AdventOfCode(2015, 16, 2, "", null)]
+    public class AdventOfCode2015162 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+        }
+    }
+}
