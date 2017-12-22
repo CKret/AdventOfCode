63a0dacad4e284f35de8a9d0e215ba0bae08bd42
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-22 22:21:17 +0100

    Completed 2017 Day 21. Completed 2017 Day 22.

---
+++ AdventOfCode/ExtensionMethods/DictionaryExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.ExtensionMethods
+{
+    public static class DictionaryExtensionMethods
+    {
+        public static void TryAdd<T1, T2>(this Dictionary<T1, T2> source, T1 key, T2 value)
+        {
+            if (!source.ContainsKey(key))
+                source.Add(key, value);
+        }
+
+        public static void TrySet<T1, T2>(this Dictionary<T1, T2> source, T1 key, T2 value)
+        {
+            if (!source.ContainsKey(key))
+                source.Add(key, value);
+            else
+                source[key] = value;
+        }
+    }
+}
