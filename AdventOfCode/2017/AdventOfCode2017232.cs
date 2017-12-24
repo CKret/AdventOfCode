195c05c6724403bfb13fed833f6c00fadf5f0e1d
Sani Singh Huttunen <sani.huttunen@gmail.com>
2017-12-24 03:13:51 +0100

    Completed 2017 Day 23.

--- AdventOfCode/2017/AdventOfCode2017232.cs
+++ AdventOfCode/2017/AdventOfCode2017232.cs
@@ -1,14 +1,45 @@
 ﻿using AdventOfCode.Core;
+using AdventOfCode.Mathematics;
 
 namespace AdventOfCode._2017
 {
     /// <summary>
+    /// --- Day 23: Coprocessor Conflagration ---
+    /// 
+    /// --- Part Two ---
+    /// 
+    /// Now, it's time to fix the problem.
+    /// 
+    /// The debug mode switch is wired directly to register a. You flip the switch,
+    /// which makes register a now start at 1 when the program is executed.
+    /// 
+    /// Immediately, the coprocessor begins to overheat. Whoever wrote this program
+    /// obviously didn't choose a very efficient implementation. You'll need to
+    /// optimize the program if it has any hope of completing before Santa needs
+    /// that printer working.
+    /// 
+    /// The coprocessor's ultimate goal is to determine the final value left in
+    /// register h once the program completes. Technically, if it had that... it
+    /// wouldn't even need to run the program.
+    /// 
+    /// After setting register a to 1, if the program were to run to completion,
+    /// what value would be left in register h?
+    /// 
     /// </summary>
-    [AdventOfCode(2017, 23, 2, "", null)]
+    [AdventOfCode(2017, 23, 2, "", 909)]
     public class AdventOfCode2017232 : AdventOfCodeBase
     {
         public override void Solve()
         {
+            var count = 0;
+            // 81 * 100 + 100000 = 108100
+            // 108100 + 17000 = 125100
+            for (var b = 108100; b <= 125100; b += 17)
+            {
+                if (!b.IsPrime()) count++;
+            }
+
+            Result = count;
         }
     }
 }
