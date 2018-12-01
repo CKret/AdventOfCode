ecf50b232cc1c6cf0eb26d5a43b694303daecd33
Sani Singh Huttunen <sani.huttunen@gmail.com>
2018-12-01 12:17:08 +0100

    Completed 2018 Day 1

---
+++ AdventOfCode/2018/AdventOfCode2018012.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2018
+{
+    /// <summary>
+    /// --- Day 1: Chronal Calibration ---
+    ///
+    /// --- Part Two ---
+    /// 
+    /// You notice that the device repeats the same frequency change list over and
+    /// over. To calibrate the device, you need to find the first frequency it
+    /// reaches twice.
+    /// 
+    /// For example, using the same list of changes above, the device would loop as
+    /// follows:
+    /// 
+    /// - Current frequency  0, change of +1; resulting frequency  1.
+    /// - Current frequency  1, change of -2; resulting frequency -1.
+    /// - Current frequency -1, change of +3; resulting frequency  2.
+    /// - Current frequency  2, change of +1; resulting frequency  3.
+    /// - (At this point, the device continues from the start of the list.)
+    /// - Current frequency  3, change of +1; resulting frequency  4.
+    /// - Current frequency  4, change of -2; resulting frequency  2, which has
+    ///   already been seen.
+    /// 
+    /// In this example, the first frequency reached twice is 2. Note that your
+    /// device might need to repeat its list of frequency changes many times before
+    /// a duplicate frequency is found, and that duplicates might be found while in
+    /// the middle of processing the list.
+    /// 
+    /// Here are other examples:
+    /// 
+    /// - +1, -1 first reaches 0 twice.
+    /// - +3, +3, +4, -2, -4 first reaches 10 twice.
+    /// - -6, +3, +8, +5, -6 first reaches 5 twice.
+    /// - +7, +7, -2, -7, -4 first reaches 14 twice.
+    /// 
+    /// What is the first frequency your device reaches twice?
+    /// </summary>
+    [AdventOfCode(2018, 1, 2, "Chronal Calibration - Part 2", 219)]
+    public class AdventOfCode2018012 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+            var frequencies = new List<int>();
+            var current = 0;
+            while (Result == null)
+            {
+                foreach (var line in File.ReadAllLines("2018\\AdventOfCode201801.txt"))
+                {
+                    var num = int.Parse(line);
+                    current += num;
+                    if (!frequencies.Contains(current))
+                    {
+                        frequencies.Add(current);
+                    }
+                    else
+                    {
+                        Result = current;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
