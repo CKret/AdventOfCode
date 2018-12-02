b758290475c06c601fe2deee8f7a5e90891345d5
Sani Singh Huttunen <sani.huttunen@gmail.com>
2018-12-02 19:28:02 +0100

    Prepared Day 201804

---
+++ AdventOfCode/2018/AdventOfCode2018041.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode._2018
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2018, 4, 1, "", null)]
+    public class AdventOfCode2018041 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+        }
+    }
+}
