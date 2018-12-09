a63adea472645936b21b550b54106c6e039f1984
Sani Singh Huttunen <sani.huttunen@gmail.com>
2018-12-09 08:59:41 +0100

    Completed Day 201809.

---
+++ AdventOfCode/ExtensionMethods/LinkedListExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.ExtensionMethods
+{
+    public static class LinkedListExtensionMethods
+    {
+        public static LinkedListNode<T> NthPrevious<T>(this LinkedListNode<T> currentNode, int count, LinkedListNode<T> lastNode)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                currentNode = currentNode?.Previous ?? lastNode;
+            }
+
+            return currentNode;
+        }
+    }
+}
