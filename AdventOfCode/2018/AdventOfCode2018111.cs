f312e52b8a77d9d972187875fb23be4e26e97b29
Sani Singh Huttunen <Sani.SinghHuttunen@svenskaspel.se>
2018-12-11 09:51:15 +0100

    Completed Day 201811.

--- AdventOfCode/2018/AdventOfCode2018111.cs
+++ AdventOfCode/2018/AdventOfCode2018111.cs
@@ -4,12 +4,69 @@ namespace AdventOfCode._2018
 {
     /// <summary>
     /// </summary>
-    [AdventOfCode(2018, 11, 1, "", null)]
+    [AdventOfCode(2018, 11, 1, "", "235,63")]
     public class AdventOfCode2018111 : AdventOfCodeBase
     {
         public override void Solve()
         {
+            var serial = 5034;
 
+            var fuelCells = new (int, int)[300, 300];
+            for (var y = 1; y <= 300; y++)
+            {
+                for (var x = 1; x <= 300; x++)
+                {
+                    fuelCells[x - 1, y - 1] = (x, y);
+                }
+            }
+
+            var max = 0;
+            var maxCoord = (0, 0);
+
+            var powers = new int[300, 300];
+
+            for (var y = 0; y < 300 - 3; y++)
+            {
+                for (var x = 0; x < 300 - 3; x++)
+                {
+                    var coord = fuelCells[x, y];
+                    var rackId = coord.Item1 + 10;
+                    var power = rackId * coord.Item2;
+                    var ser = power + serial;
+                    var mul = ser * rackId;
+                    var digit = (mul / 100) % 10;
+                    var num = digit - 5;
+
+                    powers[x, y] = num;
+
+
+                }
+            }
+
+            for (var y = 0; y < 300 - 3; y++)
+            {
+                for (var x = 0; x < 300 - 3; x++)
+                {
+                    // select 3x3
+                    var sum = 0;
+                    for (var j = 0; j < 3; j++)
+                    {
+                        for (var i = 0; i < 3; i++)
+                        {
+                            sum += powers[x + i, y + j];
+                        }
+                    }
+
+                    if (sum > max)
+                    {
+                        max = sum;
+                        maxCoord = (x + 1, y + 1);
+                    }
+
+                }
+            }
+
+            Result = maxCoord.Item1 + "," + maxCoord.Item2;
         }
     }
 }
--- AdventOfCode/2018/AdventOfCode2018112.cs
+++ AdventOfCode/2018/AdventOfCode2018112.cs
@@ -4,12 +4,54 @@ namespace AdventOfCode._2018
 {
     /// <summary>
     /// </summary>
-    [AdventOfCode(2018, 11, 2, "", null)]
+    // ReSharper disable once StringLiteralTypo
+    [AdventOfCode(2018, 11, 2, "", "229,251,16")]
     public class AdventOfCode2018112 : AdventOfCodeBase
     {
         public override void Solve()
         {
+            var serial = 5034;
 
+            var sum = new int[301][];
+            for (var i = 0; i < 301; i++)
+            {
+                sum[i] = new int[301];
+            }
+
+            var bx = int.MinValue;
+            var by = int.MinValue;
+            var bs = int.MinValue;
+            var best = int.MinValue;
+
+            for (var y = 1; y <= 300; y++)
+            {
+                for (var x = 1; x <= 300; x++)
+                {
+                    var id = x + 10;
+                    var p = id * y + serial;
+                    p = (p * id) / 100 % 10 - 5;
+                    sum[y][x] = p + sum[y - 1][x] + sum[y][x - 1] - sum[y - 1][x - 1];
+                }
+            }
+            for (var s = 1; s <= 300; s++)
+            {
+                for (var y = s; y <= 300; y++)
+                {
+                    for (var x = s; x <= 300; x++)
+                    {
+                        var total = sum[y][x] - sum[y - s][x] - sum[y][x - s] + sum[y - s][x - s];
+                        if (total > best)
+                        {
+                            best = total;
+                            bx = x;
+                            by = y;
+                            bs = s;
+                        }
+                    }
+                }
+            }
+
+            Result = (bx - bs + 1) + "," + (by - bs + 1) + "," + bs;
         }
     }
 }
