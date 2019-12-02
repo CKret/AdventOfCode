9fd9d0288ba64df10761c1e340be440d493496c5
Sani Singh Huttunen <sani@s7solutions.se>
2019-12-03 00:24:37 +0100

    Prepared for 2019 day 3.

---
+++ AdventOfCode/2019/AdventOfCode2019042.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2019
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2019, 4, 2, "", null)]
+    public class AdventOfCode2019042 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+            var data = File.ReadAllLines(@"2019\AdventOfCode201904.txt");
+        }
+    }
+}
