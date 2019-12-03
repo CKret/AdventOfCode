4cf22651e86f58091b75fb5712b1777c8a143167
Sani Singh Huttunen <sani.huttunen@profitsoftware.com>
2019-12-03 20:28:05 +0100

    Solved 2019 day 3

--- AdventOfCode/2019/AdventOfCode2019031.cs
+++ AdventOfCode/2019/AdventOfCode2019031.cs
@@ -1,16 +1,78 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AdventOfCode.Core;
+using AdventOfCode.ExtensionMethods;
+using AdventOfCode.Mathematics;
 
 namespace AdventOfCode._2019
 {
     /// <summary>
     /// </summary>
-    [AdventOfCode(2019, 3, 1, "", null)]
+    [AdventOfCode(2019, 3, 1, "", 1285)]
     public class AdventOfCode2019031 : AdventOfCodeBase
     {
         public override void Solve()
         {
             var data = File.ReadAllLines(@"2019\AdventOfCode201903.txt");
+
+            var wire1 = data[0].Split(',');
+            var wire2 = data[1].Split(',');
+
+
+            var grid1 = new Dictionary<(int, int), int>();
+            var grid2 = new Dictionary<(int, int), int>();
+
+            var x = 0;
+            var y = 0;
+            foreach(var unit in wire1)
+            {
+                var direction = unit[0];
+                var count = int.Parse(unit.Substring(1));
+
+                switch(direction)
+                {
+                    case 'U':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((++y, x), c); }
+                        break;
+                    case 'D':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((--y, x), c); }
+                        break;
+                    case 'L':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((y, --x), c); }
+                        break;
+                    case 'R':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((y, ++x), c); }
+                        break;
+                }
+            }
+
+            x = 0;
+            y = 0;
+            foreach (var unit in wire2)
+            {
+                var direction = unit[0];
+                var count = int.Parse(unit.Substring(1));
+
+                switch (direction)
+                {
+                    case 'U':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((++y, x), c); }
+                        break;
+                    case 'D':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((--y, x), c); }
+                        break;
+                    case 'L':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((y, --x), c); }
+                        break;
+                    case 'R':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((y, ++x), c); }
+                        break;
+                }
+            }
+
+            var intersections = grid1.Keys.Intersect(grid2.Keys);
+            Result = intersections.Min(p => NumberTheory.ManhattanDistance((0, 0), p));
         }
     }
 }
--- AdventOfCode/2019/AdventOfCode2019032.cs
+++ AdventOfCode/2019/AdventOfCode2019032.cs
@@ -1,16 +1,79 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AdventOfCode.Core;
+using AdventOfCode.ExtensionMethods;
 
 namespace AdventOfCode._2019
 {
     /// <summary>
     /// </summary>
-    [AdventOfCode(2019, 3, 2, "", null)]
+    [AdventOfCode(2019, 3, 2, "", 14228)]
     public class AdventOfCode2019032 : AdventOfCodeBase
     {
         public override void Solve()
         {
             var data = File.ReadAllLines(@"2019\AdventOfCode201903.txt");
+
+            var wire1 = data[0].Split(',');
+            var wire2 = data[1].Split(',');
+
+
+            var grid1 = new Dictionary<(int, int), int>();
+            var grid2 = new Dictionary<(int, int), int>();
+
+            var x = 0;
+            var y = 0;
+            var s = 0;
+            foreach (var unit in wire1)
+            {
+                var direction = unit[0];
+                var count = int.Parse(unit.Substring(1));
+
+                switch (direction)
+                {
+                    case 'U':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((++y, x), ++s); }
+                        break;
+                    case 'D':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((--y, x), ++s); }
+                        break;
+                    case 'L':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((y, --x), ++s); }
+                        break;
+                    case 'R':
+                        for (var c = 0; c < count; c++) { grid1.TryAdd((y, ++x), ++s); }
+                        break;
+                }
+            }
+
+            x = 0;
+            y = 0;
+            s = 0;
+            foreach (var unit in wire2)
+            {
+                var direction = unit[0];
+                var count = int.Parse(unit.Substring(1));
+
+                switch (direction)
+                {
+                    case 'U':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((++y, x), ++s); }
+                        break;
+                    case 'D':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((--y, x), ++s); }
+                        break;
+                    case 'L':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((y, --x), ++s); }
+                        break;
+                    case 'R':
+                        for (var c = 0; c < count; c++) { grid2.TryAdd((y, ++x), ++s); }
+                        break;
+                }
+            }
+
+            var intersections = grid1.Keys.Intersect(grid2.Keys);
+            Result = intersections.Min(p => grid1[p] + grid2[p]);
         }
     }
 }
