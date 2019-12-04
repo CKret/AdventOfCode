05506068b80f0c0ab7f0cba2285607025bb7ed16
Sani Singh Huttunen <sani.huttunen@profitsoftware.com>
2019-12-04 08:01:10 +0100

    Completed 2019 day 4. (Yes I know it's ugly but it workd).

--- AdventOfCode/2019/AdventOfCode2019041.cs
+++ AdventOfCode/2019/AdventOfCode2019041.cs
@@ -4,13 +4,71 @@ using AdventOfCode.Core;
 namespace AdventOfCode._2019
 {
     /// <summary>
+    /// --- Day 4: Secure Container ---
+    /// 
+    /// You arrive at the Venus fuel depot only to discover it's protected by a
+    /// password. The Elves had written the password on a sticky note, but someone
+    /// threw it out.
+    /// 
+    /// However, they do remember a few key facts about the password:
+    /// 
+    ///  - It is a six-digit number.
+    ///  - The value is within the range given in your puzzle input.
+    ///  - Two adjacent digits are the same (like 22 in 122345).
+    ///  - Going from left to right, the digits never decrease; they only ever
+    ///    increase or stay the same (like 111123 or 135679).
+    /// 
+    /// Other than the range rule, the following are true:
+    /// 
+    ///  - 111111 meets these criteria (double 11, never decreases).
+    ///  - 223450 does not meet these criteria (decreasing pair of digits 50).
+    ///  - 123789 does not meet these criteria (no double).
+    /// 
+    /// How many different passwords within the range given in your puzzle input
+    /// meet these criteria?
     /// </summary>
-    [AdventOfCode(2019, 4, 1, "", null)]
+    [AdventOfCode(2019, 4, 1, "How many different passwords within the range given in your puzzle input meet these criteria?", 466)]
     public class AdventOfCode2019041 : AdventOfCodeBase
     {
         public override void Solve()
         {
-            var data = File.ReadAllLines(@"2019\AdventOfCode201904.txt");
+            var data = File.ReadAllText(@"2019\AdventOfCode201904.txt");
+
+            var rangeStart = int.Parse(data.Split('-')[0]);
+            var rangeEnd = int.Parse(data.Split('-')[1]);
+
+            var count = 0;
+            for (var i = rangeStart; i <= rangeEnd; i++)
+            {
+                if (HasDouble(i) && IsDecreasing(i)) count++;
+            }
+
+            Result = count;
+        }
+
+        protected bool HasDouble(int num)
+        {
+            var str = num.ToString();
+
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1]) return true;
+            }
+
+            return false;
+        }
+
+        protected bool IsDecreasing(int num)
+        {
+            var tmp = num % 10;
+            while (num > 0)
+            {
+                num = num / 10;
+                if (num % 10 > tmp) return false;
+                tmp = num % 10;
+            }
+
+            return true;
         }
     }
 }
--- AdventOfCode/2019/AdventOfCode2019042.cs
+++ AdventOfCode/2019/AdventOfCode2019042.cs
@@ -4,13 +4,91 @@ using AdventOfCode.Core;
 namespace AdventOfCode._2019
 {
     /// <summary>
+    /// --- Day 4: Secure Container ---
+    /// 
+    /// --- Part Two ---
+    /// 
+    /// An Elf just remembered one more important detail: the two adjacent matching
+    /// digits are not part of a larger group of matching digits.
+    /// 
+    /// Given this additional criterion, but still ignoring the range rule, the
+    /// following are now true:
+    /// 
+    ///  - 112233 meets these criteria because the digits never decrease and all
+    ///    repeated digits are exactly two digits long.
+    ///  - 123444 no longer meets the criteria (the repeated 44 is part of a
+    ///    larger group of 444).
+    ///  - 111122 meets the criteria (even though 1 is repeated more than twice,
+    /// it still contains a double 22).
+    /// 
+    /// How many different passwords within the range given in your puzzle input
+    /// meet all of the criteria?
     /// </summary>
-    [AdventOfCode(2019, 4, 2, "", null)]
+    [AdventOfCode(2019, 4, 2, "How many different passwords within the range given in your puzzle input meet all of the criteria?", 292)]
     public class AdventOfCode2019042 : AdventOfCodeBase
     {
         public override void Solve()
         {
-            var data = File.ReadAllLines(@"2019\AdventOfCode201904.txt");
+            var data = File.ReadAllText(@"2019\AdventOfCode201904.txt");
+
+            var rangeStart = int.Parse(data.Split('-')[0]);
+            var rangeEnd = int.Parse(data.Split('-')[1]);
+
+            var count = 0;
+            for (var i = rangeStart; i <= rangeEnd; i++)
+            {
+                if (HasDouble(i) && IsDecreasing(i) && IsExactly2Repeating(i)) count++;
+            }
+
+            Result = count;
+        }
+
+        protected bool IsExactly2Repeating(int num)
+        {
+            var str = num.ToString();
+
+            var curr = string.Empty;
+            curr += str[0];
+
+            for (var i = 1; i < str.Length; i++)
+            {
+                if (str[i] == str[i - 1])
+                {
+                    curr += str[i];
+                }
+                else
+                {
+                    if (curr.Length == 2) return true;
+                    curr = "" + str[i];
+                }
+            }
+
+            return curr.Length == 2;
+        }
+
+        protected bool HasDouble(int num)
+        {
+            var str = num.ToString();
+
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1]) return true;
+            }
+
+            return false;
+        }
+
+        protected bool IsDecreasing(int num)
+        {
+            var tmp = num % 10;
+            while (num > 0)
+            {
+                num = num / 10;
+                if (num % 10 > tmp) return false;
+                tmp = num % 10;
+            }
+
+            return true;
         }
     }
 }
