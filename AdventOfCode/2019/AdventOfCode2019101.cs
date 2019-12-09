e7f15450cb1e5a64853d7fe15cf7dc501d64823e
Sani Singh Huttunen <sani@s7solutions.se>
2019-12-09 21:15:13 +0100

    Prepared 2019 day 10.

---
+++ AdventOfCode/2019/AdventOfCode2019101.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2019
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2019, 10, 1, "", null)]
+    public class AdventOfCode2019101 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+            var data = File.ReadAllText(@"2019\AdventOfCode201909.txt");
+        }
+    }
+}
---
+++ AdventOfCode/2019/AdventOfCode2019102.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using AdventOfCode.Core;
+
+namespace AdventOfCode._2019
+{
+    /// <summary>
+    /// </summary>
+    [AdventOfCode(2019, 10, 2, "", null)]
+    public class AdventOfCode2019102 : AdventOfCodeBase
+    {
+        public override void Solve()
+        {
+            var data = File.ReadAllText(@"2019\AdventOfCode201910.txt");
+        }
+    }
+}
