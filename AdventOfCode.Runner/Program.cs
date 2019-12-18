a63fc99bd86eaca64bb483cf58954566e27da424
Sani Singh Huttunen <sani.huttunen@profitsoftware.com>
2019-12-18 20:43:27 +0100

    Temp commit.

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -6,10 +6,10 @@ namespace AdventOfCode.Runner
     {
         static void Main(string[] args)
         {
-            var aoc1 = new AdventOfCode._2019.AdventOfCode2019171();
+            var aoc1 = new AdventOfCode._2019.AdventOfCode2019181();
             aoc1.Solve();
 
-            var aoc2 = new AdventOfCode._2019.AdventOfCode2019172();
+            var aoc2 = new AdventOfCode._2019.AdventOfCode2019182();
             aoc2.Solve();
 
             Console.WriteLine($"Part 1: {aoc1.Result}");
--- AdventOfCode/2019/AdventOfCode2019181.cs
+++ AdventOfCode/2019/AdventOfCode2019181.cs
@@ -1,5 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AdventOfCode.Core;
+using AdventOfCode.Mathematics;
+using AdventOfCode.Mathematics.PathFinding;
+using AdventOfCode.Mathematics.PathFinding.Core;
 
 namespace AdventOfCode._2019
 {
@@ -11,6 +17,26 @@ namespace AdventOfCode._2019
         public override void Solve()
         {
             var data = File.ReadAllLines(@"2019\AdventOfCode201918.txt");
+
+            var (x, y) = (0, 0);
+            var map = data
+                .ToDictionary(l => l.ToArray()
+                .ToDictionary(c => (x++, y), c => c), c => (x, y) = (0, y + 1))
+                .SelectMany(i => i.Key)
+                .ToDictionary(i => i.Key, i => i.Value);
+
+            var keys = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            var doors = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            var entrance = map.Single(p => p.Value == '@');
+            var allKeys = map.Where(p => keys.Contains(p.Value)).ToList();
+            var allDoors = map.Where(p => doors.Contains(p.Value)).ToList();
+
+            var manhattanKeys = allKeys.Select(k => NumberTheory.ManhattanDistance(entrance.Key, k.Key));
+
+            var dijkstra = Dijkstras.CalculateShortestPathBetween(entrance.Key, allKeys[0].Key, new List<PathFindingBase<(int, int)>>());
+
+
         }
     }
 }
