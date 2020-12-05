64b13d86a17cd5765590f1c83f8951f17f11c140
Sani Singh Huttunen <sani@s7solutions.se>
2020-12-05 06:45:18 +0100

    Completed 2020 Day 5.

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -10,7 +10,7 @@ var builder = new ConfigurationBuilder()
 var config = builder.Build();
 var sessionCookie = config["AdventOfCodeSessionCookie"];
 
-var aoc = new AdventOfCode._2020.AdventOfCode202004(sessionCookie);
+var aoc = new AdventOfCode._2020.AdventOfCode202005(sessionCookie);
 aoc.Solve();
 
 Console.WriteLine(aoc.Problem.Description);
