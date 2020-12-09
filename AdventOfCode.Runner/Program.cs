8d398f3f2c5a0b13e07967d8f84f980d92395d1d
Sani Singh Huttunen <sani@s7solutions.se>
2020-12-09 18:03:03 +0100

    Refacatoring and optmization of 2020 Day 08 part 2.

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -10,7 +10,7 @@ var builder = new ConfigurationBuilder()
 var config = builder.Build();
 var sessionCookie = config["AdventOfCodeSessionCookie"];
 
-var aoc = new AdventOfCode._2020.AdventOfCode202009(sessionCookie);
+var aoc = new AdventOfCode._2020.AdventOfCode202008(sessionCookie);
 aoc.Solve();
 
 Console.WriteLine(aoc.Problem.Description);
