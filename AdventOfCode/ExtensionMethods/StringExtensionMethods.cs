a3e1006cb665cd770d455c4f97922b3a9c18f083
Sani Singh Huttunen <sani@s7solutions.se>
2020-12-09 15:38:26 +0100

    Completed 2020 Day 07. Completed 2020 Day 08. Completed 2020 Day 09.

--- AdventOfCode/ExtensionMethods/StringExtensionMethods.cs
+++ AdventOfCode/ExtensionMethods/StringExtensionMethods.cs
@@ -45,5 +45,15 @@ namespace AdventOfCode.ExtensionMethods
         {
             return s.Split(new[] {splitWith}, options);
         }
+
+        public static string TrimEnd(this string input, string suffixToRemove, StringComparison comparisonType = StringComparison.CurrentCulture)
+        {
+            if (suffixToRemove != null && input.EndsWith(suffixToRemove, comparisonType))
+            {
+                return input.Substring(0, input.Length - suffixToRemove.Length);
+            }
+
+            return input;
+        }
     }
 }
