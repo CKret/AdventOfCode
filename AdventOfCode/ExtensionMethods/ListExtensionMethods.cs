768124b316893662e8ea45ff0f18401bfde79070
Sani Singh Huttunen <sani@s7solutions.se>
2020-12-17 13:52:48 +0100

    Completed 2020 Day 17.

---
+++ AdventOfCode/ExtensionMethods/ListExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.ExtensionMethods
+{
+    public static class ListExtensionMethods
+    {
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(i => (T) i.Clone()).ToList();
+        }
+    }
+}
