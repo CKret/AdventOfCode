f5e094e5c821e4216bce03289346052a02c513c7
Sani Singh Huttunen <sani.huttunen@gmail.com>
2021-12-03 10:44:07 +0100

    .gitattributes (#3)
    * Prepared 2021 day 03
    
    * Completed 2021 day 4
    
    * Refactored.
    
    * Added descriptions.
    
    * Added .cs to .gitattributes.

--- AdventOfCode/2021/AdventOfCode202103.cs
+++ AdventOfCode/2021/AdventOfCode202103.cs
@@ -7,7 +7,7 @@ namespace AdventOfCode._2021
 {
     /// <summary>
     /// </summary>
-    [AdventOfCode(2021, 3, "", 1092896, 4672151)]
+    [AdventOfCode(2021, 3, "Binary Diagnostic", 1092896, 4672151)]
     public class AdventOfCode202103 : AdventOfCodeBase
     {
         public AdventOfCode202103(string sessionCookie) : base(sessionCookie) { }
@@ -28,8 +28,7 @@ namespace AdventOfCode._2021
             for (var n = 0; n < oxygenNumbers[0].Length; n++)
             {
                 var ones = oxygenNumbers.Select(x => x[n]).Count(x => x == '1');
-                var zeroes = oxygenNumbers.Select(x => x[n]).Count(x => x == '0');
-                var mostCommon = ones >= zeroes ? '1' : '0';
+                var mostCommon = ones >= oxygenNumbers.Count - ones ? '1' : '0';
                 oxygenNumbers = oxygenNumbers.Where(x => x[n] == mostCommon).ToList();
                 if (oxygenNumbers.Count == 1) break;
             }
@@ -37,12 +36,10 @@ namespace AdventOfCode._2021
             for (var n = 0; n < co2Numbers[0].Length; n++)
             {
                 var ones = co2Numbers.Select(x => x[n]).Count(x => x == '1');
-                var zeroes = co2Numbers.Select(x => x[n]).Count(x => x == '0');
-                var leastCommon = zeroes <= ones ? '0' : '1';
+                var leastCommon = co2Numbers.Count - ones <= ones ? '0' : '1';
                 co2Numbers = co2Numbers.Where(x => x[n] == leastCommon).ToList();
                 if (co2Numbers.Count == 1) break;
             }
-
 
             return Convert.ToInt64(oxygenNumbers.Single(), 2) * Convert.ToInt64(co2Numbers.Single(), 2);
         }
