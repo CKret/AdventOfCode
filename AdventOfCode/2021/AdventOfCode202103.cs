cc56a65435c722a06a8a44465ef0474c56f3731a
Sani Singh Huttunen <sani.huttunen@gmail.com>
2021-12-03 10:58:57 +0100

    Merge (#5)
    * 2021 day 4 (#2)
    
    * Prepared 2021 day 03
    
    * Completed 2021 day 4
    
    * Refactored.
    
    * .gitattributes (#3)
    
    * Prepared 2021 day 03
    
    * Completed 2021 day 4
    
    * Refactored.
    
    * Added descriptions.
    
    * Added .cs to .gitattributes.

--- AdventOfCode/2021/AdventOfCode202103.cs
+++ AdventOfCode/2021/AdventOfCode202103.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode._2021
     /// You're minding your own business on a ship at sea when the overboard alarm
     /// goes off! You rush to see if you can help. Apparently, one of the Elves
     /// tripped and accidentally sent the sleigh keys flying into the ocean!
-    ///
+    /// 
     /// Before you know it, you're inside a submarine the Elves keep ready for
     /// situations like this. It's covered in Christmas lights(because of course
     /// it is), and it even has an experimental antenna that should be able to
