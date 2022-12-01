dd6537cf24c586904f2652bdd639f2d640fb6b67
Sani Huttunen <sani.huttunen@gmail.com>
2022-12-01 14:13:00 +0700

    Added README.md

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -11,7 +11,7 @@ var builder = new ConfigurationBuilder()
 var config = builder.Build();
 var sessionCookie = config["AdventOfCodeSessionCookie"];
 
-var aoc = new AdventOfCode._2022.AdventOfCode202201(sessionCookie);
+var aoc = new AdventOfCode._2022.AdventOfCode202202(sessionCookie);
 aoc.Solve();
 
 Console.WriteLine($"{aoc.Problem.Year} day {aoc.Problem.Day} - {aoc.Problem.Description}");
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code
+
+My solutions for each years [Advent of Code](https://adventofcode.com "Advent calendar of small programming puzzles") puzzles.
