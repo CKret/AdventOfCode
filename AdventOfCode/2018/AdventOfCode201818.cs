0a62d930ce1b76679591ac0ae11d771cfdec1261
Sani Huttunen <sani.huttunen@gmail.com>
2022-12-11 15:11:59 +0700

    Refactored 2022 day 11. Refactored 2020 day 18. Refactored 2018 day 18.

--- AdventOfCode/2018/AdventOfCode201818.cs
+++ AdventOfCode/2018/AdventOfCode201818.cs
@@ -6,133 +6,133 @@ using AdventOfCode.Core;
 
 namespace AdventOfCode._2018
 {
-    /// <summary>
-    /// </summary>
-    [AdventOfCode(2018, 18, "", 466312, 176782)]
-    public class AdventOfCode201818 : AdventOfCodeBase
-    {
-        public AdventOfCode201818(string sessionCookie) : base(sessionCookie) { }
+	/// <summary>
+	/// </summary>
+	[AdventOfCode(2018, 18, "", 466312, 176782)]
+	public class AdventOfCode201818 : AdventOfCodeBase
+	{
+		public AdventOfCode201818(string sessionCookie) : base(sessionCookie) { }
 
-        protected override object SolvePart1()
-        {
-            var data = File.ReadAllText(@"2018\AdventOfCode201818.txt").Replace(Environment.NewLine, "").ToCharArray();
+		protected override object SolvePart1()
+		{
+			var data = Input.SelectMany(x => x).ToArray();
 
-            for (var m = 0; m < 10; m++)
-            {
-                var newData = new List<char>();
-                for (var y = 0; y < 50; y++)
-                {
-                    for (var x = 0; x < 50; x++)
-                    {
-                        var current = data[y * 50 + x];
+			for (var m = 0; m < 10; m++)
+			{
+				var newData = new List<char>();
+				for (var y = 0; y < 50; y++)
+				{
+					for (var x = 0; x < 50; x++)
+					{
+						var current = data[y * 50 + x];
 
-                        // adjacent
-                        var adjacent = new List<char>();
+						// adjacent
+						var adjacent = new List<char>();
 
-                        if (y > 0 && x > 0) adjacent.Add(data[(y - 1) * 50 + x - 1]);
-                        if (y > 0) adjacent.Add(data[(y - 1) * 50 + x]);
-                        if (y > 0 && x < 49) adjacent.Add(data[(y - 1) * 50 + x + 1]);
+						if (y > 0 && x > 0) adjacent.Add(data[(y - 1) * 50 + x - 1]);
+						if (y > 0) adjacent.Add(data[(y - 1) * 50 + x]);
+						if (y > 0 && x < 49) adjacent.Add(data[(y - 1) * 50 + x + 1]);
 
-                        if (x > 0) adjacent.Add(data[y * 50 + x - 1]);
-                        if (x < 49) adjacent.Add(data[y * 50 + x + 1]);
+						if (x > 0) adjacent.Add(data[y * 50 + x - 1]);
+						if (x < 49) adjacent.Add(data[y * 50 + x + 1]);
 
-                        if (y < 49 && x > 0) adjacent.Add(data[(y + 1) * 50 + x - 1]);
-                        if (y < 49) adjacent.Add(data[(y + 1) * 50 + x]);
-                        if (y < 49 && x < 49) adjacent.Add(data[(y + 1) * 50 + x + 1]);
+						if (y < 49 && x > 0) adjacent.Add(data[(y + 1) * 50 + x - 1]);
+						if (y < 49) adjacent.Add(data[(y + 1) * 50 + x]);
+						if (y < 49 && x < 49) adjacent.Add(data[(y + 1) * 50 + x + 1]);
 
-                        if (current == '.') // Open
-                        {
-                            if (adjacent.Count(a => a == '|') >= 3)
-                                current = '|';
-                        }
-                        else if (current == '|') // Trees
-                        {
-                            if (adjacent.Count(a => a == '#') >= 3)
-                                current = '#';
-                        }
-                        else if (current == '#') // Lumberyard
-                        {
-                            if (adjacent.All(a => a != '#') || adjacent.All(a => a != '|'))
-                                current = '.';
-                        }
+						if (current == '.') // Open
+						{
+							if (adjacent.Count(a => a == '|') >= 3)
+								current = '|';
+						}
+						else if (current == '|') // Trees
+						{
+							if (adjacent.Count(a => a == '#') >= 3)
+								current = '#';
+						}
+						else if (current == '#') // Lumberyard
+						{
+							if (adjacent.All(a => a != '#') || adjacent.All(a => a != '|'))
+								current = '.';
+						}
 
-                        newData.Add(current);
-                    }
-                }
+						newData.Add(current);
+					}
+				}
 
-                data = newData.ToArray();
-            }
+				data = newData.ToArray();
+			}
 
-            var trees = data.Count(c => c == '|');
-            var lumberyards = data.Count(c => c == '#');
+			var trees = data.Count(c => c == '|');
+			var lumberyards = data.Count(c => c == '#');
 
-            return trees * lumberyards;
-        }
+			return trees * lumberyards;
+		}
 
-        protected override object SolvePart2()
-        {
-            var data = File.ReadAllText(@"2018\AdventOfCode201818.txt").Replace(Environment.NewLine, "").ToCharArray();
+		protected override object SolvePart2()
+		{
+			var data = Input.SelectMany(x => x).ToArray();
 
-            var history = new List<char[]>();
+			var history = new List<char[]>();
 
-            for (var m = 0; m < 1000000000; m++)
-            {
-                var newData = new List<char>();
-                for (var y = 0; y < 50; y++)
-                {
-                    for (var x = 0; x < 50; x++)
-                    {
-                        var current = data[y * 50 + x];
+			for (var m = 0; m < 1000000000; m++)
+			{
+				var newData = new List<char>();
+				for (var y = 0; y < 50; y++)
+				{
+					for (var x = 0; x < 50; x++)
+					{
+						var current = data[y * 50 + x];
 
-                        // adjacent
-                        var adjacent = new List<char>();
+						// adjacent
+						var adjacent = new List<char>();
 
-                        if (y > 0 && x > 0) adjacent.Add(data[(y - 1) * 50 + x - 1]);
-                        if (y > 0) adjacent.Add(data[(y - 1) * 50 + x]);
-                        if (y > 0 && x < 49) adjacent.Add(data[(y - 1) * 50 + x + 1]);
+						if (y > 0 && x > 0) adjacent.Add(data[(y - 1) * 50 + x - 1]);
+						if (y > 0) adjacent.Add(data[(y - 1) * 50 + x]);
+						if (y > 0 && x < 49) adjacent.Add(data[(y - 1) * 50 + x + 1]);
 
-                        if (x > 0) adjacent.Add(data[y * 50 + x - 1]);
-                        if (x < 49) adjacent.Add(data[y * 50 + x + 1]);
+						if (x > 0) adjacent.Add(data[y * 50 + x - 1]);
+						if (x < 49) adjacent.Add(data[y * 50 + x + 1]);
 
-                        if (y < 49 && x > 0) adjacent.Add(data[(y + 1) * 50 + x - 1]);
-                        if (y < 49) adjacent.Add(data[(y + 1) * 50 + x]);
-                        if (y < 49 && x < 49) adjacent.Add(data[(y + 1) * 50 + x + 1]);
+						if (y < 49 && x > 0) adjacent.Add(data[(y + 1) * 50 + x - 1]);
+						if (y < 49) adjacent.Add(data[(y + 1) * 50 + x]);
+						if (y < 49 && x < 49) adjacent.Add(data[(y + 1) * 50 + x + 1]);
 
-                        if (current == '.') // Open
-                        {
-                            if (adjacent.Count(a => a == '|') >= 3)
-                                current = '|';
-                        }
-                        else if (current == '|') // Trees
-                        {
-                            if (adjacent.Count(a => a == '#') >= 3)
-                                current = '#';
-                        }
-                        else if (current == '#') // Lumberyard
-                        {
-                            if (adjacent.All(a => a != '#') || adjacent.All(a => a != '|'))
-                                current = '.';
-                        }
+						if (current == '.') // Open
+						{
+							if (adjacent.Count(a => a == '|') >= 3)
+								current = '|';
+						}
+						else if (current == '|') // Trees
+						{
+							if (adjacent.Count(a => a == '#') >= 3)
+								current = '#';
+						}
+						else if (current == '#') // Lumberyard
+						{
+							if (adjacent.All(a => a != '#') || adjacent.All(a => a != '|'))
+								current = '.';
+						}
 
-                        newData.Add(current);
-                    }
-                }
+						newData.Add(current);
+					}
+				}
 
-                history.Add(data);
-                data = newData.ToArray();
-                if (history.Any(h => h.SequenceEqual(data))) break;
-            }
+				history.Add(data);
+				data = newData.ToArray();
+				if (history.Any(h => h.SequenceEqual(data))) break;
+			}
 
-            var prev = history.IndexOf(history.Single(h => h.SequenceEqual(data)));
-            var last = history.Count;
-            var repeat = last - prev;
+			var prev = history.IndexOf(history.Single(h => h.SequenceEqual(data)));
+			var last = history.Count;
+			var repeat = last - prev;
 
-            var index = prev + (1000000000 - last) % repeat;
+			var index = prev + (1000000000 - last) % repeat;
 
-            var trees = history[index].Count(c => c == '|');
-            var lumberyards = history[index].Count(c => c == '#');
+			var trees = history[index].Count(c => c == '|');
+			var lumberyards = history[index].Count(c => c == '#');
 
-            return trees * lumberyards;
-        }
-    }
+			return trees * lumberyards;
+		}
+	}
 }
---
+++ AdventOfCode/Mathematics/Expressions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Mathematics
+{
+	public static class Expressions
+	{
+		private static readonly Dictionary<string, int> DefaultOperatorPrecedence = new()
+		{
+			["*"] = 2,
+			["/"] = 2,
+			["+"] = 1,
+			["-"] = 1,
+		};
+
+		public static long EvaluateExpression(string prefix, IReadOnlyDictionary<string, int> operatorPrecedence = null)
+		{
+			operatorPrecedence ??= DefaultOperatorPrecedence;
+
+			var postfix = ToPostfix(prefix, operatorPrecedence);
+			var argStack = new Stack<long>();
+			foreach (var token in postfix)
+			{
+				switch (token)
+				{
+					case "*":
+						argStack.Push(argStack.Pop() * argStack.Pop());
+						break;
+					case "/":
+						argStack.Push(argStack.Pop() / argStack.Pop());
+						break;
+					case "+":
+						argStack.Push(argStack.Pop() + argStack.Pop());
+						break;
+					case "-":
+						argStack.Push(argStack.Pop() - argStack.Pop());
+						break;
+					default:
+						argStack.Push(long.Parse(token));
+						break;
+				}
+			}
+
+			return argStack.Pop();
+		}
+
+		public static IEnumerable<string> ToPostfix(string infix, IReadOnlyDictionary<string, int> operatorPrecedence = null)
+		{
+			operatorPrecedence ??= DefaultOperatorPrecedence;
+
+			var tokens = infix
+				.Replace("(", " ( ")
+				.Replace(")", " ) ")
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+			var stack = new Stack<string>();
+			var output = new List<string>();
+
+			foreach (var token in tokens)
+			{
+				if (operatorPrecedence.TryGetValue(token, out var op1))
+				{
+					while (stack.Count > 0 && operatorPrecedence.TryGetValue(stack.Peek(), out var op2))
+					{
+						if (op1.CompareTo(op2) <= 0)
+						{
+							output.Add(stack.Pop());
+						}
+						else
+						{
+							break;
+						}
+					}
+
+					stack.Push(token);
+				}
+				else if (token == "(")
+				{
+					stack.Push(token);
+				}
+				else if (token == ")")
+				{
+					string top;
+					while (stack.Count > 0 && (top = stack.Pop()) != "(")
+					{
+						output.Add(top);
+					}
+				}
+				else
+				{
+					output.Add(token);
+				}
+			}
+
+			while (stack.Count > 0)
+			{
+				output.Add(stack.Pop());
+			}
+
+			return output;
+		}
+	}
+}
