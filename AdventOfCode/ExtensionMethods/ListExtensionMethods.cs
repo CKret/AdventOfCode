3003ed7ba61b8d81b71d56878dbf8335b575d479
Sani Huttunen <sani.huttunen@gmail.com>
2025-11-26 17:51:56 +0700

    Updated to .NET 10. Fixed breaking changes from .NET 8 to .NET 10.

--- AdventOfCode/ExtensionMethods/ListExtensionMethods.cs
+++ AdventOfCode/ExtensionMethods/ListExtensionMethods.cs
@@ -8,12 +8,36 @@ namespace AdventOfCode.ExtensionMethods
     {
         public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
         {
-            return listToClone.Select(i => (T) i.Clone()).ToList();
+            return listToClone.Select(i => (T)i.Clone()).ToList();
         }
 
         public static void RemoveLast<T>(this IList<T> e, int count = 1)
         {
             while (count-- > 0) e.RemoveAt(e.Count - 1);
+        }
+
+        public static IEnumerable<List<T>> ChunkBy<T>(this IEnumerable<T> source, Func<T, bool> isSeparator)
+        {
+            var chunk = new List<T>();
+            foreach (var item in source)
+            {
+                if (isSeparator(item))
+                {
+                    if (chunk.Count > 0)
+                    {
+                        yield return chunk;
+                        chunk = new List<T>();
+                    }
+                }
+                else
+                {
+                    chunk.Add(item);
+                }
+            }
+            if (chunk.Count > 0)
+            {
+                yield return chunk;
+            }
         }
     }
 }
